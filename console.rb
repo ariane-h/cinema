a26dec4fe84d8716771ba75f819f74e4529b4ea0
Ariane Holford <aribomop@gmail.com>
2020-03-06 16:38:31 +0000

    add update methods

--- console.rb
+++ console.rb
@@ -7,9 +7,21 @@ require('pry-byebug')
 customer1 = Customer.new({'name' => 'Daisy Steiner', 'funds' => 20 })
 customer1.save
 
+customer1.name = 'Twist'
+customer1.update
 
 film1 = Film.new({'title' => 'Parasite', 'price' => 10})
 film1.save
 
+film1.title = 'Okja'
+film1.update
+
+film2 = Film.new({'title' => 'The Handmaiden', 'price' => 12})
+film2.save
+
+
 ticket1 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id })
 ticket1.save
+
+ticket1.film_id = film2.id
+ticket1.update
