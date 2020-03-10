4d13c04606782fe449eef6571a911e2081c1202c
Ariane Holford <aribomop@gmail.com>
2020-03-10 15:30:19 +0000

    week 4, day 2, mvp

--- controller.rb
+++ controller.rb
@@ -5,7 +5,9 @@ require_relative('models/film')
 also_reload('.models/*')
 
 get '/films' do
-  list = Film.all.map {|film| Film.new(film)}
-  @films = list.map {|film| film.title}
+  erb(:index)
+end
+
+get '/films/parasite' do
   erb(:index)
 end
